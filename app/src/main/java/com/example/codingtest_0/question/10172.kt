4448e54e95c515099a644906dcc71334aeb66258
junggyungjin <junggyungjin@naver.com>
2023-08-30 14:59:40 +0900

    백준 10172번

--- app/src/main/java/com/example/codingtest_0/question/10172.kt
+++ app/src/main/java/com/example/codingtest_0/question/10172.kt
@@ -1,5 +1,9 @@
 package com.example.codingtest_0.question
 
 fun main(args: Array<String>) {
-
+    println("|\\_/|")
+    println("|q p|   /}")
+    println("( 0 )\"\"\"\\")
+    println("|\"^\"`    |")
+    println("||_/=\\\\__|")
 }
