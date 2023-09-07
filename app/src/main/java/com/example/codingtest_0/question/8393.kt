c526d238f7a6eb95ffd0a5770b636cf001067787
junggyungjin <junggyungjin@naver.com>
2023-09-07 16:53:53 +0900

    8393 다른방법 풀기

--- app/src/main/java/com/example/codingtest_0/question/8393.kt
+++ app/src/main/java/com/example/codingtest_0/question/8393.kt
@@ -1,11 +1,7 @@
 package com.example.codingtest_0.question
 
 fun main(args: Array<String>) {
-    var sum = 0
     val n = readLine()!!.toInt().let {
-        for (i in 0 .. it) {
-            sum += i
-        }
-        println(sum)
+        println((1..it).sum())
     }
 }
