dfa28c4ba18493cae2c1b164e705caccdb8e0b89
junggyungjin <junggyungjin@naver.com>
2023-09-14 17:47:51 +0900

    백준 2439번

---
+++ app/src/main/java/com/example/codingtest_0/question/2439.kt
@@ -0,0 +1,14 @@
+package com.example.codingtest_0.question
+
+fun main() {
+
+    readln().toInt().let {
+        for (i in 1 .. it) {
+            for (j in it downTo 1) {
+                print(if (i >= j) "*" else " ")
+            }
+            println("")
+        }
+    }
+
+}
