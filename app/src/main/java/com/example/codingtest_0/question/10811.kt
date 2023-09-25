08e3851f2e9c63dd8d8771f6f3f97cfb62b54520
junggyungjin <junggyungjin@naver.com>
2023-09-25 23:57:32 +0900

    백준 10811번

--- app/src/main/java/com/example/codingtest_0/question/10811.kt
+++ app/src/main/java/com/example/codingtest_0/question/10811.kt
@@ -3,14 +3,11 @@ package com.example.codingtest_0.question
 fun main() {
     readln().split(" ").map { it.toInt() }.let {
         var (n,m)  = it[0] to it[1]
-        var array = arrayListOf<Int>()
-        for (i in 1..n) {
-            array.add(i)
-        }
+        var array = Array(n) {it+1}
         repeat(m) {
             readln().split(" ").map { it.toInt() }.let {
                 var (i,j) = it[0]-1 to it[1]
-                array.subList(i,j)
+                array.reverse(i,j)
             }
         }
         print(array.joinToString(" "))
