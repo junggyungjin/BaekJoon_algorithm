dcb04fc97866c38d1fd2ba3dc6d385b639075ec6
junggyungjin <junggyungjin@naver.com>
2023-10-08 23:13:47 +0900

    백준 25083번

---
+++ app/src/main/java/com/example/codingtest_0/question/25083.kt
@@ -0,0 +1,10 @@
+package com.example.codingtest_0.question
+
+fun main() {
+    println("         ,r'\"7")
+    println("r`-_   ,'  ,/")
+    println(" \\. \". L_r'")
+    println("   `~\\/")
+    println("      |")
+    print("      |")
+}
